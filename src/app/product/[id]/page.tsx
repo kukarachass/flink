0cffe6814d55bc2feb8aca7a44cc6d858ed2cf63
kukarachass <maksimvozniy18@gmail.com>
2025-12-20 17:54:00 +0100

    base structure seted

---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,12 @@
+interface IParams {
+    id: string;
+}
+
+export default function OffersPage( params: IParams ) {
+    const id = params.id;
+    return (
+        <div>
+            product page
+        </div>
+    )
+}
