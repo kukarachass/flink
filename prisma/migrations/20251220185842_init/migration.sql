992146a9a4233549e435562a11431ae762cede3b
kukarachass <maksimvozniy18@gmail.com>
2025-12-23 16:14:27 +0100

    prisma removed

--- prisma/migrations/20251220185842_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "SecondProduct" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "price" DOUBLE PRECISION NOT NULL,
-
-    CONSTRAINT "SecondProduct_pkey" PRIMARY KEY ("id")
-);
